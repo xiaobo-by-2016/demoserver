b4ee6e3af4b66682b67babf593771809582b86f8
mine-xiaobo <827651350@qq.com>
2018-05-10 02:32:51 +0800

    aaa

--- mysql-files/myapp.sql
+++ mysql-files/myapp.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-05-09 01:39:29
+Date: 2018-05-10 02:32:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,6 +33,23 @@ INSERT INTO `t_college_info` VALUES ('1', '1001', '西南石油大学');
 INSERT INTO `t_college_info` VALUES ('2', '1002', '四川大学');
 INSERT INTO `t_college_info` VALUES ('3', '1003', '电子科技大学');
 INSERT INTO `t_college_info` VALUES ('4', '1004', '西南科技大学');
+
+-- ----------------------------
+-- Table structure for `t_progress`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_progress`;
+CREATE TABLE `t_progress` (
+  `progress_id` int(20) DEFAULT NULL COMMENT '进度id',
+  `progress` varchar(20) DEFAULT NULL COMMENT '进度',
+  `progress_content` varchar(255) DEFAULT NULL COMMENT '进度内容',
+  `progress_time` varchar(50) DEFAULT NULL COMMENT '进度时间',
+  `progress_account` int(20) DEFAULT NULL COMMENT '关联的学生账号',
+  `progress_topic_id` int(20) DEFAULT NULL COMMENT '关联的题目id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_progress
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `t_role`
@@ -89,7 +106,7 @@ CREATE TABLE `t_user` (
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES ('9', '201305020443', null, '123456', '18428369049', null, '2', null, null);
-INSERT INTO `t_user` VALUES ('10', '201305020442', null, '123456', '18584808258', null, '1', null, null);
+INSERT INTO `t_user` VALUES ('10', '201305020442', '教师1', '1234567', '18584808258', '1001', '1', '1', '82@qq.com');
 
 -- ----------------------------
 -- Table structure for `t_vali_code`
@@ -101,10 +118,11 @@ CREATE TABLE `t_vali_code` (
   `vali_code` int(4) NOT NULL COMMENT '验证码',
   `vali_time` varchar(50) NOT NULL,
   PRIMARY KEY (`id`,`vali_code_phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_vali_code
 -- ----------------------------
 INSERT INTO `t_vali_code` VALUES ('4', '18428369049', '1762', '1525716122954');
 INSERT INTO `t_vali_code` VALUES ('5', '18584808258', '2681', '1525718001211');
+INSERT INTO `t_vali_code` VALUES ('6', '17828011784', '4712', '1525877753726');
