c5efe65e095ef6f86b6c1a24d85e55547a0ee69a
xiaobo-by-2016 <827651350@qq.com>
2018-06-01 14:23:28 +0800

    Update SmsService.js
    短信服务签名修改

--- services/SmsService.js
+++ services/SmsService.js
@@ -17,7 +17,7 @@ var SmsService = {
             Action: 'SendSms',
             TemplateParam:'{"code":' + options.templateParam + '}' ,
             PhoneNumbers: options.phoneNumbers,
-            SignName: '毕业设计进度管理系统',
+            SignName: '本科毕业设计移动服务平台',
             TemplateCode: TemplateCodeArr[options.templateIndex]
         }).then((result) => {
             console.log(result)
